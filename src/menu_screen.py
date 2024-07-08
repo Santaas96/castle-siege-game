896606bc3bf94d35af1b13f23d11024ace56884c
Santiago <santiagocortes996@hotmail.com>
2024-07-08 16:42:20 -0300

    titulo

--- src/menu_screen.py
+++ src/menu_screen.py
@@ -2,16 +2,19 @@ import pygame
 from puntajes_screen import puntajes_screen
 from quit import quit_game
 from game_screen import init_game
-from utils import dibujar_boton
+from utils import dibujar_boton, mostrar_texto
 
 def main_menu_screen(pantalla: pygame.Surface, config):
   fuente = pygame.font.Font(None, 36)
+  fuente2 = pygame.font.Font(None, 50)
   while True:
     pantalla.fill(config["BLACK"])
 
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         quit_game()
+
+    mostrar_texto(pantalla, f'CASTEL SIEGE', fuente2, coordenada=(config["SCREEN_WIDTH"] // 2, 100), color= config["GREEN"])
 
     dibujar_boton(pantalla, "JUGAR", pygame.Rect(300, 200, 200, 50), config["BLUE"], config["YELLOW"], fuente, config, init_game)
 
